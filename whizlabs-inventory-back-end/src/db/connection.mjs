4092b87b59ba700ec37826fd85164e0b28fb0556
Santho257 <santhoshkumar02553@gmail.com>
2024-11-13 20:08:55 +0530

    removed unnecessary log

--- whizlabs-inventory-back-end/src/db/connection.mjs
+++ whizlabs-inventory-back-end/src/db/connection.mjs
@@ -3,7 +3,6 @@ import {DB_NAME} from "../constants.js";
 
 const connectDB = async () => {
     try {
-        console.log(process.env.MONGO_URI);
         const instance = await mongoose.connect(`${process.env.MONGO_URI}/${DB_NAME}`);
         console.log(`MongoDB Connected Successfully to the host : ${instance.connection.host}`);        
     } catch (error) {
