9a8a6468e7ba92b85e1bbc871b13e3597f095988
Santho257 <santhoshkumar02553@gmail.com>
2024-11-15 18:15:27 +0530

    Allow Cross Origin Support to backend

--- whizlabs-inventory-back-end/src/app.js
+++ whizlabs-inventory-back-end/src/app.js
@@ -1,8 +1,10 @@
 import express from "express";
 import itemsRouter from "./routes/items.route.mjs";
+import cors from "cors";
 
 const app = express();
 app.use(express.json());
+app.use(cors({ origin: process.env.ALLOWED_ORIGIN }));
 
 app.use("/api/v1/items", itemsRouter);
 
